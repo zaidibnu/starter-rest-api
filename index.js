717450152fa452328385480b8848fb881efff5f8
dXpDevs <zaidibnu@gmail.com>
2022-11-02 20:18:53 +0700

    Redeployement #1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const db = require('cyclic-dynamodb')
-
+const APIMANGROVE = require('./repo/api')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -62,7 +62,7 @@ app.get('/:col', async (req, res) => {
 
 // Catch all handler for all other request.
 app.use('*', (req, res) => {
-  res.json({ msg: 'no route handler found' }).end()
+  res.json(APIMANGROVE())
 })
 
 // Start the server
---
+++ repo/api.js
@@ -0,0 +1,6 @@
+module.exports = function  APIMANGROVE (){
+    return {
+        version :1.0,
+        status:'Development'
+    }
+}
