8a2fdf1e25b76ae7dc2fbb9cb09acb10bc2b67a8
Zaid Ibnu <zaidibnu@gmail.com>
2023-07-11 08:32:14 +0700

    #update

--- index.js
+++ index.js
@@ -1,51 +1,73 @@
-const express = require('express');
+const express = require('express')
+const app = express()
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
 const bodyParser = require('body-parser');
-const app = express();
-const port = 3000;
-const CyclicDb = require("@cyclic.sh/dynamodb")
-const dynamodb = CyclicDb("ill-gray-squirrel-gearCyclicDB")
-app.use(bodyParser.json());
-app.get('/api/users', (req, res) => {
-  const params = {
-    TableName: 'Users' // Ganti dengan nama tabel DynamoDB Anda
-  };
 
-  dynamodb.scan(params, (error, data) => {
-    if (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
+app.use(bodyParser.json())
+
+// curl -i https://some-app.cyclic.app/myFile.txt
+app.get('*', async (req,res) => {
+  let filename = req.path.slice(1)
+
+  try {
+    let s3File = await s3.getObject({
+      Bucket: process.env.BUCKET,
+      Key: filename,
+    }).promise()
+
+    res.set('Content-type', s3File.ContentType)
+    res.send(s3File.Body.toString()).end()
+  } catch (error) {
+    if (error.code === 'NoSuchKey') {
+      console.log(`No such key ${filename}`)
+      res.sendStatus(404).end()
     } else {
-      res.json(data.Items);
+      console.log(error)
+      res.sendStatus(500).end()
     }
-  });
-});
-
-app.post('/api/users', (req, res) => {
-  const { id, name } = req.body;
-
-  if (!id || !name) {
-    res.status(400).send('Bad Request');
-    return;
   }
+})
 
-  const params = {
-    TableName: 'Users', // Ganti dengan nama tabel DynamoDB Anda
-    Item: {
-      id: { S: id },
-      name: { S: name }
-    }
-  };
 
-  dynamodb.putItem(params, (error) => {
-    if (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    } else {
-      res.status(201).send('User created successfully');
-    }
-  });
-});
+// curl -i -XPUT --data '{"k1":"value 1", "k2": "value 2"}' -H 'Content-type: application/json' https://some-app.cyclic.app/myFile.txt
+app.put('*', async (req,res) => {
+  let filename = req.path.slice(1)
 
+  console.log(typeof req.body)
+
+  await s3.putObject({
+    Body: JSON.stringify(req.body),
+    Bucket: process.env.BUCKET,
+    Key: filename,
+  }).promise()
+
+  res.set('Content-type', 'text/plain')
+  res.send('ok').end()
+})
+
+// curl -i -XDELETE https://some-app.cyclic.app/myFile.txt
+app.delete('*', async (req,res) => {
+  let filename = req.path.slice(1)
+
+  await s3.deleteObject({
+    Bucket: process.env.BUCKET,
+    Key: filename,
+  }).promise()
+
+  res.set('Content-type', 'text/plain')
+  res.send('ok').end()
+})
+
+// /////////////////////////////////////////////////////////////////////////////
+// Catch all handler for all other request.
+app.use('*', (req,res) => {
+  res.sendStatus(404).end()
+})
+
+// /////////////////////////////////////////////////////////////////////////////
+// Start the server
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+  console.log(`index.js listening at http://localhost:${port}`)
+})
